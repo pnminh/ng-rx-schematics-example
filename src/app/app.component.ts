9dd6b5ab8d64c12f74283401bd6f22e6dd2f855f
minhpham <minh.pham@dhigroupinc.com>
2018-12-07 14:34:04 -0600

    add reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as reducers from './store/reducers';
+import { Store } from '@ngrx/store';
+import * as authActions from './store/actions/auth.actions';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-rx-schematics-example';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.store.dispatch(new authActions.LoadAuths());
+  }
+  constructor(private store: Store<reducers.State>) {}
 }
